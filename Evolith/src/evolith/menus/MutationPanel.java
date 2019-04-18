ee66bb0270a8e0838aff38f8a3bcebbfca01e430
vsapiens <erick.frank@icloud.com>
2019-04-18 16:22:47 -0500

    mutation panel active fixed

--- Evolith/src/evolith/menus/MutationPanel.java
+++ Evolith/src/evolith/menus/MutationPanel.java
@@ -72,104 +72,102 @@ public class MutationPanel extends Menu implements Commons {
         hmap.put(2, "II");
         hmap.put(3, "III");
         hmap.put(4, "IV");
-        
+
     }
 
     @Override
     public void tick() {
-        
+
         /*
         if(organism.isDead())
         {
             active = false;
         }
-        */
+         */
     }
 
     @Override
     public void render(Graphics g) {
-        if(active){
-        g.drawImage(Assets.mutation_menu, x, y, width, height, null);
-        g.setColor(Color.red);
-        
-        g.drawRect(x + MUTATION_PANEL_WIDTH / 2 - 250, y + 500, 240, 60);
-        g.drawRect(x + MUTATION_PANEL_WIDTH / 2 + 250, y + 500, 240, 60);
-     
-        for (int i = 0; i < organism.getOrgMutations().getMutations().size(); i++) {
-            
-            g.setColor(Color.WHITE);
-            
-            g.fillRect(x + 495, y + 81 + 115 * i, (int) 60 * organism.getStrength() / MAX_STRENGTH, 15);
-            g.fillRect(x + 495, y + 106 + 115 * i, (int) 60 * organism.getMaxHealth() / MAX_SURVIVABILITY, 15);
-            g.fillRect(x + 613, y + 81 + 115 * i, (int) 60 * organism.getSpeed() / MAX_SPEED, 15);
-            g.fillRect(x + 613, y + 106 + 115 * i, (int) 60 * organism.getStealth() / MAX_STEALTH, 15);
-            
-            int j = 0;
-            
-             for (int k = 0; k < organism.getOrgMutations().getMutations().get(i).size(); k++) {
+        if (active) {
+            g.drawImage(Assets.mutation_menu, x, y, width, height, null);
+            g.setColor(Color.red);
+
+            g.drawRect(x + MUTATION_PANEL_WIDTH / 2 - 250, y + 500, 240, 60);
+            g.drawRect(x + MUTATION_PANEL_WIDTH / 2 + 250, y + 500, 240, 60);
+
+            for (int i = 0; i < organism.getOrgMutations().getMutations().size(); i++) {
+
+                g.setColor(Color.WHITE);
+
+                g.fillRect(x + 495, y + 81 + 115 * i, (int) 60 * organism.getStrength() / MAX_STRENGTH, 15);
+                g.fillRect(x + 495, y + 106 + 115 * i, (int) 60 * organism.getMaxHealth() / MAX_SURVIVABILITY, 15);
+                g.fillRect(x + 613, y + 81 + 115 * i, (int) 60 * organism.getSpeed() / MAX_SPEED, 15);
+                g.fillRect(x + 613, y + 106 + 115 * i, (int) 60 * organism.getStealth() / MAX_STEALTH, 15);
+
+                int j = 0;
+
+                for (int k = 0; k < organism.getOrgMutations().getMutations().get(i).size(); k++) {
                     if (organism.getOrgMutations().getMutations().get(i).get(j).isActive()) {
-                        j  = k;
+                        j = k;
                     }
-              }
-                    j=0;
-                    if (j == organism.getOrgMutations().getMutations().get(i).size() - 1) {
-                        g.drawImage(Assets.mutation_max_tier, 380, 110, y + 395, x + 30 + i * 115, null);
-                        g.setColor(Color.red);
-                        g.drawRect(380, 110, y + 395, x + 30 + i * 115);
-                    } else {
-                        g.setColor(Color.WHITE);
-                        g.setFont(fontEvolve);
-                       // g.drawString((String)hmap.get(organism.getOrgMutations().getMutations().get(i).get(j).getTier()) , x + 468, y + 48 + i*118);
-                        g.drawString(organism.getOrgMutations().getMutations().get(i).get(j).getName(), x + 501, y + 48 + i*118);
+                }
+                j = 0;
+                if (j == organism.getOrgMutations().getMutations().get(i).size() - 1) {
+                    g.drawImage(Assets.mutation_max_tier, 380, 110, y + 395, x + 30 + i * 115, null);
+                    g.setColor(Color.red);
+                    g.drawRect(380, 110, y + 395, x + 30 + i * 115);
+                } else {
+                    g.setColor(Color.WHITE);
+                    g.setFont(fontEvolve);
+                    // g.drawString((String)hmap.get(organism.getOrgMutations().getMutations().get(i).get(j).getTier()) , x + 468, y + 48 + i*118);
+                    g.drawString(organism.getOrgMutations().getMutations().get(i).get(j).getName(), x + 501, y + 48 + i * 118);
 
-                        if (organism.getOrgMutations().getMutations().get(i).get(j).getStrength() != 0) {
+                    if (organism.getOrgMutations().getMutations().get(i).get(j).getStrength() != 0) {
 
-                            if (organism.getOrgMutations().getMutations().get(i).get(j).getStrength() < 0) {
-                                g.setColor(Color.RED);
-                                g.fillRect(x + 495 + ((int) 60* (organism.getStrength() + organism.getOrgMutations().getMutations().get(i).get(j).getStrength())/MAX_STRENGTH) , y + 81 + 115 * i, (int) 60 *Math.abs(organism.getOrgMutations().getMutations().get(i).get(j).getStrength()) / MAX_STRENGTH, 15);
-                            }
-                            else {
-                                g.setColor(Color.GREEN);
-                                g.fillRect(x + 495 + organism.getStrength(), y + 81 + 115 * i, (int) 60 * organism.getOrgMutations().getMutations().get(i).get(j).getStrength()/ MAX_STRENGTH, 15);
-                            }
-                            
-                            if (organism.getOrgMutations().getMutations().get(i).get(j).getMaxHealth() < 0) {
-                                g.setColor(Color.RED);
-                                g.fillRect(x + 495 + organism.getMaxHealth() + organism.getOrgMutations().getMutations().get(i).get(j).getStrength(), y + 106 + 115 * i,(int) 60 * Math.abs(organism.getOrgMutations().getMutations().get(i).get(j).getMaxHealth()) / MAX_SURVIVABILITY, 15);
-                            }
-                            else {
-                                g.setColor(Color.GREEN);
-                                g.fillRect(x + 495 + organism.getMaxHealth(), y + 106 + 115 * i,(int) 60 * organism.getOrgMutations().getMutations().get(i).get(j).getMaxHealth() / MAX_SURVIVABILITY, 15);
-                            }
-                             
-                            if (organism.getOrgMutations().getMutations().get(i).get(j).getStealth() < 0) {
-                                g.setColor(Color.RED);
-                                g.fillRect(x + 613 + organism.getStealth() + organism.getOrgMutations().getMutations().get(i).get(j).getStealth(), y + 106 + 115 * i, (int) 60 *Math.abs(organism.getOrgMutations().getMutations().get(i).get(j).getStealth()) / MAX_STEALTH, 15);
-                            }
-                            else {
-                                g.setColor(Color.GREEN);
-                                g.fillRect(x + 613 + organism.getStealth(), y + 106 + 115 * i, (int) 60 * organism.getOrgMutations().getMutations().get(i).get(j).getStealth() / MAX_STEALTH, 15);
-                            }
-                            
-                            if (organism.getOrgMutations().getMutations().get(i).get(j).getSpeed() < 0) {
-                                g.setColor(Color.RED);
-                                g.fillRect(x + 613 + organism.getSpeed() + organism.getOrgMutations().getMutations().get(i).get(j).getSpeed(), y + 81 + 115 * i, (int) 60 *Math.abs(organism.getOrgMutations().getMutations().get(i).get(j).getSpeed()) / MAX_SPEED, 15);
-                            }
-                            else {
-                                g.setColor(Color.GREEN);
-                                g.fillRect(x + 613 + organism.getSpeed(), y + 81 + 115 * i, (int) 60* organism.getOrgMutations().getMutations().get(i).get(j).getSpeed() / MAX_SPEED, 15);
-                            }
+                        if (organism.getOrgMutations().getMutations().get(i).get(j).getStrength() < 0) {
+                            g.setColor(Color.RED);
+                            g.fillRect(x + 495 + (int) 60 * ((organism.getStrength() + organism.getOrgMutations().getMutations().get(i).get(j).getStrength()) / MAX_STRENGTH), y + 81 + 115 * i, (int) 60 * -1 * organism.getOrgMutations().getMutations().get(i).get(j).getStrength() / MAX_STRENGTH, 15);
+                        } else {
+                            g.setColor(Color.GREEN);
+                            g.fillRect(x + 495 + (int) 60 * organism.getStrength() / MAX_STRENGTH, y + 81 + 115 * i, (int) 60 * organism.getOrgMutations().getMutations().get(i).get(j).getStrength() / MAX_STRENGTH, 15);
                         }
                     }
-
+                    if (organism.getOrgMutations().getMutations().get(i).get(j).getMaxHealth() != 0) {
+                        if (organism.getOrgMutations().getMutations().get(i).get(j).getMaxHealth() < 0) {
+                            g.setColor(Color.RED);
+                            g.fillRect(x + 495 + (int) 60 * ((organism.getMaxHealth() + organism.getOrgMutations().getMutations().get(i).get(j).getStrength()) / MAX_SURVIVABILITY), y + 106 + 115 * i, (int) 60 * -1 * organism.getOrgMutations().getMutations().get(i).get(j).getMaxHealth() / MAX_SURVIVABILITY, 15);
+                        } else {
+                            g.setColor(Color.GREEN);
+                            g.fillRect(x + 495 + (int) 60 * organism.getMaxHealth() / MAX_SURVIVABILITY, y + 106 + 115 * i, (int) 60 * organism.getOrgMutations().getMutations().get(i).get(j).getMaxHealth() / MAX_SURVIVABILITY, 15);
+                        }
+                    }
+                    if (organism.getOrgMutations().getMutations().get(i).get(j).getStealth() != 0) {
+                        if (organism.getOrgMutations().getMutations().get(i).get(j).getStealth() < 0) {
+                            g.setColor(Color.RED);
+                            g.fillRect(x + 613 + (int) 60 * ((organism.getStealth() + organism.getOrgMutations().getMutations().get(i).get(j).getStealth()) / MAX_STEALTH), y + 106 + 115 * i, (int) 60 * -1 * organism.getOrgMutations().getMutations().get(i).get(j).getStealth() / MAX_STEALTH, 15);
+                        } else {
+                            g.setColor(Color.GREEN);
+                            g.fillRect(x + 613 + (int) 60 * organism.getStealth()/MAX_STEALTH, y + 106 + 115 * i, (int) 60 * organism.getOrgMutations().getMutations().get(i).get(j).getStealth() / MAX_STEALTH, 15);
+                        }
+                    }
+                    if (organism.getOrgMutations().getMutations().get(i).get(j).getSpeed() != 0) {
+                        if (organism.getOrgMutations().getMutations().get(i).get(j).getSpeed() < 0) {
+                            g.setColor(Color.RED);
+                            g.fillRect(x + 613 + (int) 60 * ((organism.getSpeed() + organism.getOrgMutations().getMutations().get(i).get(j).getSpeed()) / MAX_SPEED), y + 81 + 115 * i, (int) 60 * -1 * organism.getOrgMutations().getMutations().get(i).get(j).getSpeed() / MAX_SPEED, 15);
+                        } else {
+                            g.setColor(Color.GREEN);
+                            g.fillRect(x + 613 + (int) 60 * organism.getSpeed()/MAX_SPEED, y + 81 + 115 * i, (int) 60 * organism.getOrgMutations().getMutations().get(i).get(j).getSpeed() / MAX_SPEED, 15);
+                        }
+                    }
                 }
+            }
 
+        }
 
         for (int i = 0; i < buttons.size(); i++) {
             buttons.get(i).render(g);
         }
 
-    }
     }
 
     public boolean isActive() {
@@ -179,6 +177,5 @@ public class MutationPanel extends Menu implements Commons {
     public void setActive(boolean active) {
         this.active = active;
     }
-    
 
 }
