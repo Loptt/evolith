4eaaa3796e37e4ddd4a6898e3750bfb1506ab84c
vsapiens <erick.frank@icloud.com>
2019-04-18 19:13:22 -0500

    mutation panel active fixed

--- Evolith/src/evolith/menus/MutationPanel.java
+++ Evolith/src/evolith/menus/MutationPanel.java
@@ -115,7 +115,7 @@ public class MutationPanel extends Menu implements Commons {
                 int j = 0;
 
                 for (int k = 0; k < organism.getOrgMutations().getMutations().get(selection-1).size(); k++) {
-                    if (organism.getOrgMutations().getMutations().get(i).get(j).isActive()) {
+                    if (organism.getOrgMutations().getMutations().get(selection-1).get(j).isActive()) {
                         j = k;
                     }
                 }
